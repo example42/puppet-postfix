d1bae6c179e070eeac430324028d1a9b016a4f85
Mathieu Parent <mathieu.parent@nantesmetropole.fr>
2012-09-05 16:02:57 +0200

    Support for specifing the maps as a hash

--- manifests/map.pp
+++ manifests/map.pp
@@ -1,16 +1,28 @@
-# Define: postfix::map
+# = Define: postfix::map
 #
 # Adds a postfix lookup table
 #
-# Usage:
+# == Usage:
 # postfix::map { 'canonical':
 #   source => 'puppet:///modules/example42/postfix/canonical'
 # }
 #
+# postfix::map { 'transport':
+#   template => 'example42/postfix/transport.erb'
+# }
+#
+# postfix::map { 'virtual':
+#   maps => {
+#     'user1@virtual-alias.example.org' => 'address1',
+#     'user2@virtual-alias.example.org' => ['address2', 'address3'],
+#   }
+# }
+#
 define postfix::map(
-  $source              = params_lookup( 'source' ),
-  $template            = params_lookup( 'template' ),
-  $path = "${postfix::config_dir}/${name}",
+  $source   = params_lookup( 'source' ),
+  $template = params_lookup( 'template' ),
+  $maps     = params_lookup( 'maps' ),
+  $path     = "${postfix::config_dir}/${name}",
 ) {
   include postfix
 
@@ -20,7 +32,10 @@ define postfix::map(
   }
 
   $manage_file_content = $template ? {
-    ''        => undef,
+    ''        => $maps ? {
+      ''      => undef,
+      default => template('postfix/map.erb'),
+      },
     default   => template($template),
   }
 
