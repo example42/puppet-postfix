e40cf3f67ff1017246e3f42483251db1f1bf5acb
Martin Alfke <ma@example42.com>
2015-08-03 14:42:33 +0200

    fix spec tests on puppet 2.6
    - add fact operatingsystemrelease to spec

--- spec/classes/postfix_spec.rb
+++ spec/classes/postfix_spec.rb
@@ -4,7 +4,10 @@ describe 'postfix' do
 
   let(:title) { 'postfix' }
   let(:node) { 'rspec.example42.com' }
-  let(:facts) { { :ipaddress => '10.42.42.42' } }
+  let(:facts) { {
+      :ipaddress => '10.42.42.42',
+      :operatingsystemrelease => '6.6'
+  } }
 
   describe 'Test standard installation' do
     it { should contain_package('postfix').with_ensure('present') }
